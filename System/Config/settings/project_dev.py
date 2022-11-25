528563aae7dc2829d451679e48db9e90e4b255ba
diklios <47837462+diklios5768@users.noreply.github.com>
2022-11-25 16:05:50 +0800

    v0.3.5版本

---
+++ System/Config/settings/project_dev.py
@@ -0,0 +1,27 @@
+# -*- encoding: utf-8 -*-
+"""
+@File Name      :   project_dev.py
+@Create Time    :   2022/11/25 15:54
+@Description    :   
+@Version        :   
+@License        :   MIT
+@Author         :   diklios
+@Contact Email  :   diklios5768@gmail.com
+@Github         :   https://github.com/diklios5768
+@Blog           :   
+@Motto          :   All our science, measured against reality, is primitive and childlike - and yet it is the most precious thing we have.
+"""
+__auth__ = 'diklios'
+
+from .project import *
+
+ALLOWED_HOSTS = ['127.0.0.1']
+
+# 阿里云配置
+ALIBABACLOUD_OSS_ENDPOINT = ALIBABACLOUD_OSS_ENDPOINT_TEST
+ALIBABACLOUD_OSS_BUCKET_NAME = ALIBABACLOUD_OSS_BUCKET_NAME_TEST
+ALIBABACLOUD_OSS_BUCKET_DOMAIN = ALIBABACLOUD_OSS_BUCKET_DOMAIN_TEST
+
+# 顺丰快递配置
+SF_EXPRESS_URL = SF_EXPRESS_URL_TEST
+SF_EXPRESS_SAMPLE_APP_CHECKWORD = SF_EXPRESS_SAMPLE_APP_CHECKWORD_TEST
