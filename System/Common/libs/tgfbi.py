d4c36d9f94c93016d7f014a6c6b69e781db8b994
diklios <47837462+diklios5768@users.noreply.github.com>
2022-11-29 11:18:18 +0800

    优化合同判断逻辑，完善管理员登录的兼容性

--- System/Common/libs/tgfbi.py
+++ System/Common/libs/tgfbi.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 """
-@File Name      :   __init__.py
-@Create Time    :   2022/11/13 17:53
+@File Name      :   tgfbi.py
+@Create Time    :   2022/11/28 16:06
 @Description    :   
 @Version        :   
 @License        :   MIT
@@ -12,3 +12,14 @@
 @Motto          :   All our science, measured against reality, is primitive and childlike - and yet it is the most precious thing we have.
 """
 __auth__ = 'diklios'
+
+tgfbi_contract_settings = {
+    'JMF': {
+        'project_id': 'PSI20220003JMF',
+        'product_name': '角膜营养不良检测-基础版',
+    },
+    'JMA': {
+        'project_id': 'PSI20220004JMA',
+        'product_name': '角膜营养不良检测-基础版',
+    }
+}
